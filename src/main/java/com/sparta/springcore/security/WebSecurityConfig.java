4062db762413f1cd92403eee505f922690edcb77
idoburnish <doyun7433@naver.com>
2021-09-14 14:36:01 +0900

    add singUp

---
+++ src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.sparta.springcore.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.authorizeRequests()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/user/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                // 그 외 모든 요청은 인증과정 필요
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                //로그인이 필요할 때 필요한 페이지의 위치 지정
+                .loginPage("/user/login")
+                .loginProcessingUrl("/user/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
